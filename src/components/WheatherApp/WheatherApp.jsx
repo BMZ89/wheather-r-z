cbc5b0bd6c651d7202164684232cba192bdae047
Mohamed Zakaria <belgaila.mohamed.zakaria@gmail.com>
2023-01-15 00:15:40 +0100

    7 days push

--- src/components/WheatherApp/WheatherApp.jsx
+++ src/components/WheatherApp/WheatherApp.jsx
@@ -25,28 +25,15 @@ export default function WheatherApp() {
     if (search.trim() !== "") {
       setStartUpdate(true);
       fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search}?unitGroup=uk&key=VJ3DZUDUFNBGMJT5A8CCQNU3B&contentType=json`
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search}/next7days?unitGroup=uk&key=VJ3DZUDUFNBGMJT5A8CCQNU3B&contentType=json`
       )
         .then((response) => response.json())
         .then((result) => {
           setFetchedData(result);
         });
     }
-    // console.log(fetchedData.days[0]);
-    // console.log(fetchedData.days[0]);
   };
-  // console.log(fetchedData.days[0].temp);
-  // const Array = [fetchedData.days[0]];
-  // console.log(Array);
-  //   const daysArray = [
-  //   fetchedData.days[2],
-  //   fetchedData.days[3],
-  //   fetchedData.days[4],
-  //   fetchedData.days[5],
-  // ];
-  // console.log(Array);
-  // console.log(fetchedData);
-  // const date=
+
   //2) USING USEFFECT TO UPDATE THE INFOS WITH EVERY NEW SUBMIT OF THE USER
   useEffect(() => {
     if (startUpdate) {
@@ -58,45 +45,12 @@ export default function WheatherApp() {
       console.log(fetchedData.days[0].datetime);
       console.log(fetchedData.days[0].temp);
       console.log(fetchedData.days[0]);
+
       // FORECAST
-      // const boucle =()=> {
-      //   for (i=1; i<6, i++) {
-      //   setTemp(fetchedData.days[i].temp);
-      //   console.log(temp);
-      // }
-      // };
-      // console.log(fetchedData.days);
       setDaysArray(fetchedData.days);
-
-      // array.map((day, index) =()=> {
-      //   day = fetchedData.days[index]
-      //   console.log(day);
-      //     // return (
-      //     //   fetchedData.days[index]
-      //     // )
-      // }
-      // )
-
-      // setTempForecast(fetchedData.days[1].temp);
-      // setIconsForecast(fetchedData.days[1].icon);
-      // setDescriptionForecast(fetchedData.days[1].decription);
     }
-    // console.log(fetchedData.days[0].datetime);
-    // console.log(fetchedData.days[0].temp);
-    // console.log(fetchedData.days[0]);
   }, [fetchedData]);
   console.log(daysArray);
-  // console.log(dayDate);
-  // const daysArray = [
-  //   fetchedData.days[2],
-  //   fetchedData.days[3],
-  //   fetchedData.days[4],
-  //   fetchedData.days[5],
-  // ];
-  // console.log(daysArray);
-
-  // console.log(daysArray);
-  // console.log(daysArray[2]);
 
   return (
     <div className="weather-app-container">
@@ -110,20 +64,15 @@ export default function WheatherApp() {
         <button type="submit">Search</button>
       </form>
 
-      <div className="days-container">
-        <DayInfo
-          city={search}
-          temperature={temp}
-          dayDate={dayDate}
-          srcIcon={`icons/${icons}.png`}
-          description={description}
-        />
+      <div className="center-container">
+        <h2>{search}</h2>
       </div>
 
       <div className="forecast-container">
         {daysArray.map((dayItem, index) => {
           return (
             <DayInfo
+              dayDate={daysArray[index].datetime}
               temperature={daysArray[index].temp}
               srcIcon={`icons/${daysArray[index].icon}.png`}
               description={daysArray[index].description}
